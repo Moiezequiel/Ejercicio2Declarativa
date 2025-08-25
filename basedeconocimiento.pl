55b3d2e60cb187fac8c5405cdf5b8178335c464f
JoseBernabe123 <00038121@uca.edu.sv>
2025-08-25 16:44:32 -0600

    basedeconocimiento

---
+++ basedeconocimiento.pl
@@ -0,0 +1,76 @@
+% Aldea de Rivoria
+hijo(clara, don_esteban).
+hijo(clara, dona_mariela).
+
+hijo(rodolfo, don_esteban).
+hijo(rodolfo, dona_mariela).
+
+hijo(ismael, don_esteban).
+hijo(ismael, dona_mariela).
+
+hijo(sofia, clara).
+hijo(sofia, andres).
+
+hijo(lucas, rodolfo).
+hijo(lucas, teresa).
+
+hijo(martina, rodolfo).
+hijo(martina, teresa).
+
+% Aldea de Montelar
+hijo(felipe, don_alonso).
+hijo(felipe, dona_beatriz).
+
+hijo(catalina, don_alonso).
+hijo(catalina, dona_beatriz).
+
+hijo(emilia, don_alonso).
+hijo(emilia, dona_beatriz).
+
+% Aldea de Durania
+hijo(mateo, ).
+hijo(irene, ).
+
+% Matrimonios
+matrimonio(clara, andres).
+matrimonio(rodolfo, teresa).
+matrimonio(catalina, andres).
+matrimonio(mateo, irene).
+
+% Amistades
+amistad(ismael, hector).
+
+% Aprendices
+aprendiz(lucas, hector, herreria).
+
+% Rivoria oficios
+oficio(hector, herrero).
+oficio(teresa, tejedora).
+oficio(clara, maestra).
+oficio(rodolfo, herrero).
+
+% Montelar oficios
+oficio(catalina, curandera).
+oficio(felipe, explorador).
+oficio(emilia, ceramista).
+
+% Durania oficios
+oficio(mateo, agricultor).
+oficio(irene, mercader).
+
+% Rivalidad 
+rival(rodolfo, felipe).
+rival(clara, catalina).
+
+% Alianzas
+alianza(rivoria, durania).
+pacto_defensa(rivoria, durania).
+
+% Festividades
+festividad(rivoria, fiesta_del_rio).
+festividad(montelar, ceremonia_de_la_luna).
+festividad(durania, feria_del_grano).
+
+espiritu(rivoria, agua).
+espiritu(montelar, montana).
+espiritu(durania, tierra).
