5212dffc056953fec2dc5f82d0064e301d157aed
ChrMlg <54544751+ChrMlg@users.noreply.github.com>
2025-09-17 17:18:38 -0600

    Create Clase6.pl

---
+++ Clase6.pl
@@ -0,0 +1,36 @@
+% Árbol genealógico griego 
+arbol_mitologico(
+  arbol(caos,
+    arbol(gea,
+      arbol(cronos,
+        arbol(zeus,
+          arbol(hefesto, nil, 
+            arbol(ares, nil, 
+              arbol(dionisio, nil, 
+                arbol(hermes, nil,
+                  arbol(atenea, nil,
+                    arbol(artemisa, nil,
+                      arbol(apolo, nil,
+                        arbol(afrodita, nil, nil)
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          ),
+          nil
+        ),
+        arbol(hera, nil, nil)
+      ),
+      nil
+    ),
+    arbol(uranos,
+      nil,
+      arbol(rea,
+        arbol(poseidon, nil, nil),
+        arbol(hades, nil, nil)
+      )
+    )
+  )
+).
