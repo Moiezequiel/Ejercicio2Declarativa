8f6da6e97193b016a83d618c6549681431310214
ChrMlg <54544751+ChrMlg@users.noreply.github.com>
2025-11-30 21:19:11 -0600

    Added Ejercicio Aves and distributed files into directories
    no further changes were made and

---
+++ Ejercicio Aves/aves.pl
@@ -0,0 +1,10 @@
+ % Aves
+ ave(loro).
+ ave(aguila).
+ ave(pinguino).
+ % Qué come cada ave
+ come(loro, semillas).
+ come(aguila, carne).
+ come(pinguino, pescado).
+ % Regla
+ comida(X) :- ave(X), X \= aguila.
